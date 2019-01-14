c16ac8f7134188b99b5a58f257dc4e049a7f16ad
henryosi <henry_osi@yahoo.com>
2019-01-14 09:41:10 -0700

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,7 @@
+console.log("Up and running!");
+var cardOne = "Queen of Diamonds";
+var cardTwo = "Queen of Hearts";
+var cardThree = "King of Diamonds";
+var cardFour = "King of Hearts";
+console.log("User flipped" + " " + cardOne);
+console.log("User flipped" + " " + cardTwo);
