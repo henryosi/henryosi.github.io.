01474c85863b91d1b1a374c0909c9d3f2ca27a96
henryosi <henry_osi@yahoo.com>
2019-01-15 20:30:38 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,10 +1,16 @@
 console.log("Up and running!");
-var cards = ['Queen', 'Queen', 'King', 'King'];
+var cards = [{rank: "queen", suit:"hearts", cardImage: "images/queen-of-hearts.png"},
+{rank: "queen", suit: "diamonds", cardImage: "images/queen-of-diamonds.png"},
+{rank: "king", suit: "hearts", cardImage: "images/king-of-hearts.png"},
+{rank: "king", suit: "diamonds", cardImage: "images/king-of-diamonds.png"}];
 var cardsInPlay = [];
-var checkForMatch = function() {if (cardsInPlay.length === 2) {if (cardsInPlay[0] === cardsInPlay[1]) alert("You found a match"); else {alert("Sorry,try again");}};}
+var checkForMatch = function() {if (cardsInPlay.length === 2) 
+{if (cardsInPlay[0] === cardsInPlay[1]) alert("You found a match"); else {alert("Sorry,try again");}};}
 var flipCard = function (cardId) {
-cardsInPlay.push(cardId);
-console.log("User flipped" + " " + cards[cardId])
+cardsInPlay.push(cards[cardId].rank);
+console.log("User flipped" + " " + cards[cardId].rank);
+console.log(cards[cardId].cardImage);
+console.log(cards[cardId].suit);
 checkForMatch();
 };
 flipCard(0);
